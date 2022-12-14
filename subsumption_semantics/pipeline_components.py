7673b9a504b10b161b48c05c83bbfcea85024b77
Adam Tomkins <artomkins@googlemail.com>
2022-12-14 18:07:18 +0000

    Remove Neurosymbolic

--- subsumption_semantics/pipeline_components.py
+++ subsumption_semantics/pipeline_components.py
@@ -4,7 +4,7 @@ from spacy.language import Language
 
 from subsumption_semantics.similarity_metrics import (
     late_interaction_similarity,
-    neurosymbolic_similarity,
+    #neurosymbolic_similarity,
     complex_similarity
 )
 
@@ -20,16 +20,16 @@ class LateInteractionSimilarity:
         return doc
 
 
-@Language.factory("neurosymbolic_similarity", default_config={"data": {}})
-class NeuroSymbolicSimilarity:
-    def __init__(self, nlp, name: str, data: Dict):
-        self.name = name
-        self.data = data
-
-    def __call__(self, doc):
-        doc.user_hooks["similarity"] = neurosymbolic_similarity
-        doc.user_span_hooks["similarity"] = neurosymbolic_similarity
-        return doc
+#@Language.factory("neurosymbolic_similarity", default_config={"data": {}})
+#class NeuroSymbolicSimilarity:
+#    def __init__(self, nlp, name: str, data: Dict):
+#        self.name = name
+#        self.data = data
+#
+#    def __call__(self, doc):
+#        doc.user_hooks["similarity"] = neurosymbolic_similarity
+#        doc.user_span_hooks["similarity"] = neurosymbolic_similarity
+#        return doc
 
 
 @Language.factory("complex_similarity", default_config={})
